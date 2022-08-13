5cb13d31cd7f505164cd2329af24a1dacc0f4417
Maqhobela <maqkao@gmail.com>
2022-08-13 12:43:32 +0200

    Dealing with Enums, decaration, initialization, iteration, enums properties and methods

--- src/com/opticsolutions/StringCreation.java
+++ src/com/opticsolutions/StringCreation.java
@@ -4,27 +4,41 @@ import java.util.Arrays;
 
 public class StringCreation {
 	public static void main(String[] args) {
-		System.out.println("===== String methods =====");
-		System.out.println();
+//		System.out.println("===== String methods =====");
+//		System.out.println();
+//		
+//		String s = " hello ";
+//		System.out.println("s.length(): " + s.length());					// 7
+//		System.out.println("s.contains(\"he\"): " + s.contains("he"));		// true
+//		System.out.println("s.isEmpty: " + s.isEmpty());					// false
+//		System.out.println("s.toUpperCase(): " + s.toUpperCase());			//  HELLO
+//		System.out.println("s.startsWith(\"h\"): " + s.startsWith("h"));	// false
+//		System.out.println("s.endsWith(\" \"): "+ s.endsWith(" "));			// true
+//		System.out.println("s.replace(\"ll\", \"LL\"): " + s.replace("ll", "LL"));	// heLLo
+//		System.out.println("s.trim(): " + s.trim());						// hello
+//		System.out.println("s.strip(): " + s.strip());						// hello
+//		System.out.println("s.substring(0, 3): " + s.substring(0, 3));		//  he
+//		System.out.println("Arrays.toString(s.getBytes()): " 
+//						+ Arrays.toString(s.getBytes()));	// [32, 104, 101, 108, 108, 111, 32]
+//		System.out.println("Arrays.toString(s.toCharArray()): " 
+//						+ Arrays.toString(s.toCharArray()));	// [ , h, e, l, l, o,  ]
+//		System.out.println("s.charAt(1): " + s.charAt(1));					// h
+//		System.out.println("Arrays.toString(s.split(\"e\")): " 
+//						+ Arrays.toString(s.split("e")));					// [ h, llo ]
+//		System.out.println(s);
 		
-		String s = " hello ";
-		System.out.println("s.length(): " + s.length());					// 7
-		System.out.println("s.contains(\"he\"): " + s.contains("he"));		// true
-		System.out.println("s.isEmpty: " + s.isEmpty());					// false
-		System.out.println("s.toUpperCase(): " + s.toUpperCase());			//  HELLO
-		System.out.println("s.startsWith(\"h\"): " + s.startsWith("h"));	// false
-		System.out.println("s.endsWith(\" \"): "+ s.endsWith(" "));			// true
-		System.out.println("s.replace(\"ll\", \"LL\"): " + s.replace("ll", "LL"));	// heLLo
-		System.out.println("s.trim(): " + s.trim());						// hello
-		System.out.println("s.strip(): " + s.strip());						// hello
-		System.out.println("s.substring(0, 3): " + s.substring(0, 3));		//  he
-		System.out.println("Arrays.toString(s.getBytes()): " 
-						+ Arrays.toString(s.getBytes()));	// [32, 104, 101, 108, 108, 111, 32]
-		System.out.println("Arrays.toString(s.toCharArray()): " 
-						+ Arrays.toString(s.toCharArray()));	// [ , h, e, l, l, o,  ]
-		System.out.println("s.charAt(1): " + s.charAt(1));					// h
-		System.out.println("Arrays.toString(s.split(\"e\")): " 
-						+ Arrays.toString(s.split("e")));					// [ h, llo ]
-		System.out.println(s);
+		System.out.println("abc");
+	     String cde = "cde";
+	     System.out.println("abc" + cde);
+	     String c = "abc".substring(2,3);
+	     System.out.println(c);
+	     String d = cde.substring(1, 2);
+	     System.out.println(d);
+	     
+	     System.out.println();
+	     System.out.println("*================= More methods");
+	     System.out.println();
+	     
+	     System.out.println("codePointAt(2) " + "aBc".codePointAt(1));
 	}
 }
