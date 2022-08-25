6fd3303fe04878b1bc18e8a97672543c35606491
Maqhobela <maqkao@gmail.com>
2022-08-25 14:18:32 +0200

    Sorting element using functional interface predicate and fix to some project config files

---
+++ src/com/opticsolutions/functionalInterface/SortWordsInArray.java
@@ -0,0 +1,27 @@
+package com.opticsolutions.functionalInterface;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortWordsInArray {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner =  new Scanner(System.in);
+		System.out.println("Enter world separated by space");
+		String line = scanner.nextLine();
+		
+		String[] words = line.split("\\s+");
+		
+		Arrays.sort(words, (s1, s2) -> {
+			int result = s2.length() - s1.length();
+			if (result == 0) {
+				result = s1.compareToIgnoreCase(s2);
+			}
+			return result;
+		});
+		
+		for (String word : words) {
+			System.out.print(word + " ");
+		}
+	}
+}
