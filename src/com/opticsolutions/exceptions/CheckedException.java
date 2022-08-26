bc3456ef63f8261b8b680421cbbc56d597629539
Maqhobela <maqkao@gmail.com>
2022-08-26 11:21:55 +0200

    Checked exception that will throw error at compile time

---
+++ src/com/opticsolutions/exceptions/CheckedException.java
@@ -0,0 +1,10 @@
+package com.opticsolutions.exceptions;
+
+
+import java.nio.file.Paths;
+import java.nio.file.Files;
+public class CheckedException {
+	public static void main(String[] args) {
+//		Files.readAllLines(Paths.get("some file.txt"));
+	}
+}
