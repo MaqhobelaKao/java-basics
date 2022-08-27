c13a2e02208cdf7beba2a8d53efd6be5f4060907
Maqhobela <maqkao@gmail.com>
2022-08-27 02:29:56 +0200

    Implemeted two functions (add & clear)

--- src/com/opticsolutions/jcf/lists/impl/DefaultMyList.java
+++ src/com/opticsolutions/jcf/lists/impl/DefaultMyList.java
@@ -1,33 +1,34 @@
+package com.opticsolutions.jcf.lists.impl;
+
 import java.util.Iterator;
 
 import com.opticsolutions.jcf.lists.MyList;
 
 public class DefaultMyList implements MyList {
+
 	/**
-     * Pointer to first node.
-     */
+	 * Pointer to first node.
+	 */
 	private Node first;
-	
-	  /**
-     * Pointer to last node.
-     */
+
+	/**
+	 * Pointer to last node.
+	 */
 	private Node last;
-	
+
 	/**
 	 * Total amount of elements in object of MyListImpl.
 	 */
 	private int size;
-	
-	
+
 	/**
-     * Appends the specified element to the end of this list (optional
-     * operation).
-     *
-     * @param e element to be appended to this list
-     * @throws NullPointerException if the specified element is null and this
-     *         list does not permit null elements
-     */
-	
+	 * Appends the specified element to the end of this list (optional operation).
+	 *
+	 * @param e element to be appended to this list
+	 * @throws NullPointerException if the specified element is null and this list
+	 *                              does not permit null elements
+	 */
+
 	@Override
 	public Iterator<Object> iterator() {
 		// TODO Auto-generated method stub
@@ -36,14 +37,31 @@ public class DefaultMyList implements MyList {
 
 	@Override
 	public void add(Object e) {
-		// TODO Auto-generated method stub
-		
+		final Node lastNode = last;
+		final Node newNode = new Node(lastNode, e, null);
+		last = newNode;
+		if (lastNode == null) {
+			first = newNode;
+		} else {
+			lastNode.next = newNode;
+		}
+        size++;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		Node head = first;
 		
+		while(head != null) {
+			Node next = head.next;
+			head.data = null;
+			head.next = null;
+			head.previous = null;
+			head = next;
+		}
+		last = null;
+		first = null;
+		size = 0;
 	}
 
 	@Override
@@ -75,25 +93,28 @@ public class DefaultMyList implements MyList {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
+	/**
+	 * Nested class which store information about next and previous elements in
+	 * container MyList and data of this Node
+	 */
+
 	private static class Node {
 		private Object data;
 		private Node next;
 		private Node previous;
-		
+
 		public Node(Node previous, Object data, Node next) {
 			this.next = next;
 			this.previous = previous;
 			this.data = data;
 		}
-			
+
 		@Override
 		public String toString() {
 			return data.toString();
 		}
-		
-		
-		
-	} 
+
+	}
 
 }
